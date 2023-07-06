b65232afeab5f18dfba14fc19384d72a5c4a6794
Aidan-Z <aidanzamfir@gmail.com>
2023-07-06 09:38:48 -0300

    changed to main characters, created dict

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pandas as pd
+import pprint as pp
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from webdriver_manager.chrome import ChromeDriverManager
 
-URL = "https://adventuretime.fandom.com/wiki/List_of_episodes"
+URL = "https://adventuretime.fandom.com/wiki/Category:Main_Characters"
 
 driver = webdriver.Chrome()
 driver.get(URL)
 
-# data = driver.find_element(By.CSS_SELECTOR, '.table-wide-inner') #cant re-find this class in html source code....
-# eps = data.find_elements(By.TAG_NAME, 'a') #only gives 46-56......
-data = driver.find_elements(By.CLASS_NAME, "wds-tab__content wds-is-current")
-# for i in data:
-#     print(i.find_elements(By.TAG_NAME, 'a'))
+main_characters = driver.find_elements(By.CLASS_NAME, "category-page__member-link")
 
+main_chars = []
+for i in main_characters:
+    name = i.text
+    link = i.get_attribute('href')
+    main_chars.append({'name': name, 'url': link})
+
+pp.pprint(main_chars)
+print(" ")
 
