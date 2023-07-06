444096ec75b0dc8c24e15a06a93aa5a28da1c3db
Aidan-Z <aidanzamfir@gmail.com>
2023-07-06 09:59:54 -0300

    pre merge

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 import pandas as pd
 import pprint as pp
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from scraper import Scrapper
 
-URL = "https://adventuretime.fandom.com/wiki/Category:Main_Characters"
+find = Scrapper()
+find.get_characters()
 
-driver = webdriver.Chrome()
-driver.get(URL)
-
-main_characters = driver.find_elements(By.CLASS_NAME, "category-page__member-link")
-
-main_chars = []
-for i in main_characters:
-    name = i.text
-    link = i.get_attribute('href')
-    main_chars.append({'name': name, 'url': link})
-
-pp.pprint(main_chars)
-print(" ")
+pp.pprint(find.main_characters)
 
---
+++ scraper.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+URL = "https://adventuretime.fandom.com/wiki/Category:Main_Characters"
+class Scrapper:
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.main_characters = []
+        self.episodes = []
+
+    def get_characters(self):
+        self.driver.get(URL)
+        main_chars = self.driver.find_elements(By.CLASS_NAME, "category-page__member-link")
+
+        self.main_characters = [{'name': i.text, 'url': i.get_attribute('href')} for i in main_chars]
+
+
+    def get_episodes(self):
+        for i in self.main_characters:
+            self.driver.get(i['url'])
