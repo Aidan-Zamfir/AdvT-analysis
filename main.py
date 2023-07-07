c1a9208f8108dfeedb68f3e2a77961fda29b4506
Aidan-Z <aidanzamfir@gmail.com>
2023-07-07 08:36:13 -0300

    scraper complete

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from scraper_characters import CharacterScraper
 from scraper_transcripts import TextScraper
-
+import os
 import pandas as pd
-
 import spacy
 from spacy import displacy
-
 import networkx as nx
-
 import matplotlib.pyplot as plt
+
 
 NER = spacy.load(("en_core_web_sm"))
 
-find_character = CharacterScraper()
-collect_text = TextScraper()
+# find_character = CharacterScraper()
+# find_character.scrape()
 
-find_character.scrape()
-collect_text.launch()
+#Use when transcript text files (in data folder) DONT exist:
+# collect_text = TextScraper()
+# collect_text.launch()
 
-
-
-
-
+all_episodes = [i for i in os.scandir('data') if '.txt' in i.name]
+ep1 = all_episodes[0]
+ep_text = open(ep1).read()
+print(ep_text)
