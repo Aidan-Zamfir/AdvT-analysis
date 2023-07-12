09723b1291fa24fcbf47b03084e3dc4df3fe94bf
Aidan-Z <aidanzamfir@gmail.com>
2023-07-12 16:19:18 -0300

    fixed list-comp issue

--- char_data.py
+++ char_data.py
@@ -5,7 +5,6 @@ import os
 NER = spacy.load("en_core_web_sm")
 CHARACTER_DATAFRAME = pd.read_csv("character_list.csv")
 
-
 class CharacterData:
 
     def __int__(self):
@@ -25,17 +24,30 @@ class CharacterData:
 
     def episode_dataframe(self):
         """Create data frame with only sentences which contain
-        a character name using NLP"""
+        a character name using spaCy"""
 
         sentence_entity_list = []
         self.entity_list = []
 
         for i in self.doc.sents:
-            self.entity_list = [i.text for i in i.ents]
+            for j in i.ents:
+                self.entity_list.append(j.text)
+            # self.entity_list = [j.text for j in i.ents] #problem here -> empty list, [] always being saved last
             sentence_entity_list.append({'sentence': i, 'character': self.entity_list})
 
         sentence_ent_df = pd.DataFrame(sentence_entity_list)
-        self.sentence_dataframe = sentence_ent_df[sentence_ent_df['character'].map(len) > 0]
+        print(sentence_ent_df)
+        sentence_ent_df['character'] = sentence_ent_df['sentence'].apply(lambda x: self.filter_names(self.entity_list['character'], CHARACTER_DATAFRAME))
+        print(sentence_ent_df)
+        # self.sentence_dataframe = sentence_ent_df[sentence_ent_df['character'].map(len) > 0]
+        # print(self.sentence_dataframe)
+
+
+    def filter_names(self, ent_list, char_df):
+        """Filter out non main character name entities"""
+
+        return [i for i in ent_list if i in list(char_df.character_name)]
+
 
 
     def get_names(self):
--- main.py
+++ main.py
@@ -24,4 +24,4 @@ import matplotlib.pyplot as plt
 #STEP 2:
 x = CharacterData()
 x.from_episode()
-x.get_names()
+# x.get_names()
